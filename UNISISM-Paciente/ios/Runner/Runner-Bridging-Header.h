118a95821d5fbb649b4213ea357f493866986fd9
Mateus Henrique Santana Vieira <mateushenrivieira@gmail.com>
2026-08-10 10:12:43 -0300

    Configura cabeçalhos modulares e importações nativas do iOS para o Xcode

--- UNISISM-Paciente/ios/Runner/Runner-Bridging-Header.h
+++ UNISISM-Paciente/ios/Runner/Runner-Bridging-Header.h
@@ -1,1 +1,5 @@
 #import "GeneratedPluginRegistrant.h"
+#if __has_include(<flutter_local_notifications/FlutterLocalNotificationsPlugin.h>)
+#import <flutter_local_notifications/FlutterLocalNotificationsPlugin.h>
+#endif
+
